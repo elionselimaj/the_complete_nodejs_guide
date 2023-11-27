80d5372a4790430edc5d0bccd16c3c51fd27a7ed
Elion Selimaj <eselimaj@softjourn.com>
2023-11-28 00:38:43 +0100

    using express router

--- app.js
+++ app.js
@@ -4,21 +4,16 @@ const http = require('http');
 const express = require('express');
 const bodyParser = require('body-parser');
 
+const adminRoutes = require('./src/routes/admin');
+const shopRoutes = require('./src/routes/shop');
+
 // this is to create function from express which handles a lot of login
 const app = express();
 app.use(bodyParser.urlencoded({extended: false})); // it registers a middleware
 
-app.use('/add-product', (req, res,next) => {
-    res.send('<form action="/product" method="POST"><input type="text" name="title">Add Product</form>')
-}); // allows to add middleware function
+app.use(adminRoutes);
+app.use(shopRoutes);
 
-app.post('/product', (req, res,next) => { //.get fires only GET requests
-    res.redirect('/')
-});
-
-app.use('/', (req, res,next) => {
-    res.send('<h1>Hello Express</h1>')
-});
 
 //app is created once
 const server = http.createServer(app); // creates a server
