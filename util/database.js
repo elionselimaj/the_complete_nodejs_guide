385a18d9f9e94cdd39d3aa343d067c6dc5e4f581
Elion Selimaj <eselimaj@softjourn.com>
2023-12-01 15:33:13 +0100

    sequelize & relations

--- util/database.js
+++ util/database.js
@@ -1,11 +1,8 @@
-const mysql = require('mysql2');
+const Sequelize = require('sequelize');
 
-// connecting with sequelize database
-const pool = mysql.createPool({
-    host: 'localhost',
-    user: 'root',
-    database: 'node_complete',
-    password: 'Test1234!!'
+const sequelize = new Sequelize('node_complete', 'root', 'Test1234!!', {
+  dialect: 'mysql',
+  host: 'localhost'
 });
 
-module.exports = pool.promise();
+module.exports = sequelize;
