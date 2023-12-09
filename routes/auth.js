1fb1c38ec80c40a8be3e1f5ad9656a4d96c3c1be
Elion Selimaj <elionselimaj@gmail.com>
2023-12-09 16:00:45 +0100

    authentication

--- routes/auth.js
+++ routes/auth.js
@@ -10,7 +10,7 @@ router.get('/signup', authController.getSignup);
 
 router.post('/login', authController.postLogin);
 
-router.post('/signup', authController.posSignup);
+router.post('/signup', authController.postSignup);
 
 router.post('/logout', authController.postLogout);
 
